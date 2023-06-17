f36e9fa6bf95011e8cf4b4f7ba3dbac2205fbcfe
Vishal <vg539616@gmail.com>
2023-06-17 14:44:46 +0530

    Initial Commit

---
+++ components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="absolute bottom-0 w-full pb-1 pt-1 text-md flex flex-col justify-between px-2 items-center text-white bg-indigo-600 h-16">
+      <h1 className="font-semibold tracking-wide">
+        Copyright © 2022 EventsForYou
+      </h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ data/events.js
@@ -0,0 +1,322 @@
+const ongoingEvents = [
+  {
+    eventId: '1',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '2',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '3',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '4',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '5',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '6',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+];
+
+const upcomingEvents = [
+  {
+    eventId: '7',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '8',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '9',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '10',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '11',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+  {
+    eventId: '12',
+    imageUrl:
+      'https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    eventName: 'INFERNO',
+    institute: 'National Institute of Karnataka',
+  },
+];
+
+const registeredEvents = [
+  {
+    eventId: 1,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-28',
+  },
+  {
+    eventId: 2,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-12-30',
+    endDate: '2022-01-01',
+  },
+  {
+    eventId: 3,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-12-28',
+    endDate: '2021-12-30',
+  },
+  {
+    eventId: 4,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 5,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 6,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 7,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 8,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 9,
+    eventName: 'Inferno',
+    institute: 'National Institute of Technology Karnataka',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+];
+
+const listedEvents = [
+  {
+    eventId: 1,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-28',
+  },
+  {
+    eventId: 2,
+    eventName: 'Inferno',
+    startDate: '2021-12-30',
+    endDate: '2022-01-01',
+  },
+  {
+    eventId: 3,
+    eventName: 'Inferno',
+    startDate: '2021-12-28',
+    endDate: '2021-12-30',
+  },
+  {
+    eventId: 4,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 5,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 6,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 7,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 8,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+  {
+    eventId: 9,
+    eventName: 'Inferno',
+    startDate: '2021-01-26',
+    endDate: '2021-01-26',
+  },
+];
+
+const registeredStudents = [
+  {
+    id: 1,
+    first_name: 'Harsh',
+    last_name: 'Jain',
+    event_name: 'Inferno',
+    email: 'harshkawadia8.hk@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-01-26',
+  },
+  {
+    id: 2,
+    first_name: 'Mukesh',
+    last_name: 'Patel',
+    event_name: 'Inferno',
+    email: 'mukesh@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-01-28',
+  },
+  {
+    id: 3,
+    first_name: 'Vishal',
+    last_name: 'Gupta',
+    event_name: 'Inferno',
+    email: 'vishal@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-01-26',
+  },
+  {
+    id: 4,
+    first_name: 'Shivam',
+    last_name: 'Verma',
+    event_name: 'Inferno',
+    email: 'shivamV@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-03-26',
+  },
+  {
+    id: 5,
+    first_name: 'Himank',
+    last_name: 'Pathak',
+    event_name: 'Inferno',
+    email: 'himank@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-01-26',
+  },
+  {
+    id: 6,
+    first_name: 'Lakshay',
+    last_name: 'Patel',
+    event_name: 'Inferno',
+    email: 'lakshay@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-01-26',
+  },
+  {
+    id: 7,
+    first_name: 'Raushan',
+    last_name: 'Kumar',
+    event_name: 'Inferno',
+    email: 'raushan@gmail.com',
+    institute: 'National Institute of Technology Karnataka',
+    registration_date: '2021-02-26',
+  },
+];
+
+export function getEventById(eventId) {
+  if (eventId === '1') {
+    return {
+      eventId: '1',
+      eventName: 'Inferno',
+      institute: 'National Institute of Technology Karnataka',
+      state: 'Karnataka',
+      degree: 'MCA',
+      email: 'harshkawadia@nitk.edu.in',
+      startDate: '2021-02-26',
+      endDate: '2021-02-29',
+      minimumSemesterAllowed: 1,
+      modeOfConduct: 'Online',
+      description:
+        'This is the event which requires all the technical skills and coding skills as well. It allows you to have an exposure to the competitive environmnet in you field.',
+      uploadDate: '2021-02-25',
+    };
+  }
+}
+
+export function getAllEvents() {
+  return ongoingEvents;
+}
+
+export function getOngoingEvents() {
+  return ongoingEvents;
+}
+
+export function getUpcomingEvents() {
+  return upcomingEvents;
+}
+
+export function getRegisteredEvents(userId) {
+  return registeredEvents;
+}
+
+export function getListedEvents(nit_id) {
+  return listedEvents;
+}
+
+export function getRegisteredStudents(eventId) {
+  return registeredStudents;
+}
